c8bf1b9ad0bee1c394d2b5fb09c384e5156dba52
Ilya Sokol <innocentbeing911@gmail.com>
2024-10-15 22:16:03 +0300

    Change preprocessing method name

--- src/Main.java
+++ src/Main.java
@@ -26,12 +26,12 @@ public class Main {
     }
 
     public static void main(String[] args) {
-        prefilterImages();
+        preprocessImages();
         matchTemplate();
         createHists();
     }
 
-    private static void prefilterImages() {
+    private static void preprocessImages() {
         try {
             // Загрузка исходных изображений
             Mat targetSrc = Images.loadImage(PATH + "\\src\\target.jpg");
